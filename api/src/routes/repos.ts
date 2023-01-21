21c761d35ba8c9d27264a32a902c1a2eb61fc033
Shaik Wasef <wasef@regology.com>
2023-01-20 21:54:55 -0500

    feat : Add API for /repos end point

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,21 @@
 import { Router, Request, Response } from 'express';
-
+import { Repo } from '../models/Repo';
+import axios from 'axios';
+import fs from 'fs';
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
-
-  res.status(200);
-
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    const { data }: { data: Repo[] } = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    const reposData: Buffer = fs.readFileSync('./data/repos.json');
+    const responseData = [...JSON.parse(reposData.toString()), ...data].filter(
+      (dataItem: Repo) => !dataItem.fork
+    );
+    res.status(200).json(responseData);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
 });
