2ac8b9ac1c56464c68e8a32fa19cd82fc58faa40
Shaik Wasef <wasef@regology.com>
2023-01-21 23:04:27 -0500

    feat : Implemented promise chanining in git repo api code

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,21 +1,32 @@
 import { Router, Request, Response } from 'express';
 import { Repo } from '../models/Repo';
 import axios from 'axios';
-import fs from 'fs';
+import fs from 'fs/promises';
 export const repos = Router();
+const lodash = require('lodash');
 
 repos.get('/', async (_: Request, res: Response) => {
-  res.setHeader('Content-Type', 'application/json');
-  try {
-    const { data }: { data: Repo[] } = await axios.get(
-      'https://api.github.com/users/silverorange/repos'
-    );
-    const reposData: Buffer = fs.readFileSync('./data/repos.json');
-    const responseData = [...JSON.parse(reposData.toString()), ...data].filter(
-      (dataItem: Repo) => !dataItem.fork
-    );
-    res.status(200).json(responseData);
-  } catch (err) {
-    res.status(500).send('Server error');
-  }
+	res.setHeader('Content-Type', 'application/json');
+	const gitDataPromise = axios
+		.get('https://api.github.com/users/silverorange/repos')
+		.then((response) => {
+			return response.data;
+		});
+	const fileDataPromise = fs.readFile('./data/repos.json').then((response) => {
+		return JSON.parse(response.toString());
+	});
+	const promises = [gitDataPromise, fileDataPromise];
+	Promise.all(promises)
+		.then((resp) => {
+			const responseData = lodash
+				.flatten(resp)
+				.filter((dataItem: Repo) => !dataItem.fork);
+			res.status(200).json(responseData);
+		})
+		.catch((err: Error) => {
+			res.status(500).json({
+				status: 500,
+				message: err.message,
+			});
+		});
 });
