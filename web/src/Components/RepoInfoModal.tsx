d0e2b567e0239b010e4071d6d6845520329e0ea7
Shaik Wasef <wasef@regology.com>
2023-01-22 15:18:37 -0500

    fix : Add Modal content component separately

--- web/src/Components/RepoInfoModal.tsx
+++ web/src/Components/RepoInfoModal.tsx
@@ -1,19 +1,14 @@
-import Typography from '@mui/material/Typography';
 import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
 import { IRepo } from '../interfaces/apiInterfaces/repo.interface';
 import { Commit } from '../interfaces/apiInterfaces/commit.interface';
 import useApi from '../helpers/hooks/use-api';
 import { apiEndPoints } from '../helpers/api-helper';
-import ReactMarkdown from 'react-markdown';
 import useApiMarkDown from '../helpers/hooks/use-api-markdown';
 import CloseIcon from '@mui/icons-material/Close';
 import Styles from '../Styles/Components/repo-info-modal.module.scss';
-import CircularProgress from '@mui/material/CircularProgress';
-import { IApiError } from '../interfaces/use-api.interface';
-import ErrorComponent from './ErrorComponent';
+import { ModalContent } from './ModalContent';
 
 interface PropsInterface {
   open: boolean;
@@ -37,53 +32,13 @@ export default function RepoInfoModal(props: PropsInterface) {
           <CloseIcon />
         </IconButton>
       </DialogTitle>
-      {getContentComponent(
-        commitData,
-        markDownContent,
-        loading,
-        error,
-        markDownContentError
-      )}
+      <ModalContent
+        commitData={commitData}
+        markDownContent={markDownContent}
+        loading={loading}
+        error={error}
+        markDownContentError={markDownContentError}
+      />
     </Dialog>
   );
-}
-
-function getContentComponent(
-  commitData: Commit | null,
-  markDownContent: string,
-  loading: boolean,
-  error?: IApiError,
-  markDownContentError?: IApiError
-) {
-  if (loading) {
-    return <CircularProgress className={Styles.loaderClass} />;
-  }
-  if (error) {
-    return <ErrorComponent error={error} />;
-  }
-  if (commitData) {
-    return (
-      <DialogContent>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          <span className={Styles.headings}>Author's name : </span>
-          {commitData.commit.author.name}
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          <span className={Styles.headings}>Commit date : </span>
-          {new Date(commitData.commit.author.date).toLocaleString()}
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          <span className={Styles.headings}>Commit : </span>
-          {commitData.commit.message}
-        </Typography>
-        {markDownContent ? (
-          <ReactMarkdown>{markDownContent}</ReactMarkdown>
-        ) : markDownContentError ? (
-          <ErrorComponent error={markDownContentError} />
-        ) : (
-          <div />
-        )}
-      </DialogContent>
-    );
-  }
 }
