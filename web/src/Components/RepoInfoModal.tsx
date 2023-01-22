c3267b8f3a6e86239450cb4d8036c815361ec230
Shaik Wasef <wasef@regology.com>
2023-01-21 19:48:18 -0500

    feat : Add close button on dialog box

--- web/src/Components/RepoInfoModal.tsx
+++ web/src/Components/RepoInfoModal.tsx
@@ -1,27 +1,30 @@
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
 import { IRepo } from '../interfaces/apiInterfaces/repo.interface';
 import { Commit } from '../interfaces/apiInterfaces/commit.interface';
 import useApi from '../helpers/hooks/use-api';
 import { apiEndPoints } from '../helpers/api-helper';
 import ReactMarkdown from 'react-markdown';
 import useApiMarkDown from '../helpers/hooks/use-api-markdown';
+import CloseIcon from '@mui/icons-material/Close';
 import Styles from '../Styles/Components/repo-info-modal.module.scss';
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 'fit-content',
-  height: 400,
-  overflow: 'scroll',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+// const style = {
+//   position: 'absolute' as 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 'fit-content',
+//   height: 400,
+//   overflow: 'scroll',
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
 
 interface PropsInterface {
   open: boolean;
@@ -42,8 +45,13 @@ export default function RepoInfoModal(props: PropsInterface) {
   return (
     <div>
       {commitData && (
-        <Modal open={open} onClose={handleClose}>
-          <Box sx={style}>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle className={Styles.dialogHeader}>
+            <IconButton onClick={handleClose} color="primary">
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
             <Typography id="modal-modal-title" variant="h6" component="h2">
               <span className={Styles.headings}>Author's name : </span>
               {commitData.commit.author.name}
@@ -53,8 +61,8 @@ export default function RepoInfoModal(props: PropsInterface) {
               {commitData.commit.message}
             </Typography>
             <ReactMarkdown>{markDownContent}</ReactMarkdown>
-          </Box>
-        </Modal>
+          </DialogContent>
+        </Dialog>
       )}
     </div>
   );
